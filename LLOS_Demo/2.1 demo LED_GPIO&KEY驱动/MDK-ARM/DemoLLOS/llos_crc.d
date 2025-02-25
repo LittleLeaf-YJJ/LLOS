7fdf1b4f1b6a44873021801d93090ba45580d3d8
LittleLeaf <2246925209@qq.com>
2025-02-25 18:19:48 +0800

    动态内存迭代

--- LLOS_Demo/2.1 demo LED_GPIO&KEY驱动/MDK-ARM/DemoLLOS/llos_crc.d
+++
@@ -1,5 +0,0 @@
-demollos/llos_crc.o: ..\..\..\LLOS\drivers\llos_crc.c \
-  ..\..\..\LLOS\drivers\llos_crc.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdbool.h
--- LLOS_Demo/2.2 demo DS18B20驱动/MDK-ARM/DemoLLOS/taskds18b20.d
+++
@@ -1,9 +0,0 @@
-demollos/taskds18b20.o: ..\General_Files\apps\taskDS18B20.c \
-  ..\General_Files\apps\taskDS18B20.h ..\..\..\LLOS\kernel\llos.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdio.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\string.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdbool.h \
-  C:\Keil_v5\ARM\ARMCLANG\include\stdlib.h \
-  ..\..\..\LLOS\kernel\llos_conf.h ..\General_Files\hal\hal_gpio.h \
-  ..\..\..\LLOS\drivers\llos_DS18B20.h
